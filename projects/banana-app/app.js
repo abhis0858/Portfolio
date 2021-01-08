a28dfd0ecf96cce711401d273e7e80e655bc93c5
Abhishek pandey <50267963+abhis0858@users.noreply.github.com>
2021-01-08 16:16:20 +0530

    Update app.js

--- projects/banana-app/app.js
+++ projects/banana-app/app.js
@@ -1,12 +1,11 @@
 
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-var serverURL = "https://api.funtranslations.com/translate/";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
 function getTranslationURL(input) {
@@ -21,6 +20,7 @@ function errorHandler(error) {
 
 function clickHandler() {
     var inputText = txtInput.value; // taking input
+    console.log(inputText);
 
     // calling server for processing
     fetch(getTranslationURL(inputText))
