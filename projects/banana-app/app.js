71219f89f08726d5d914207c8b0a31b90ac2dfe0
Abhishek pandey <50267963+abhis0858@users.noreply.github.com>
2021-01-08 16:28:47 +0530

    Update app.js

--- projects/banana-app/app.js
+++ projects/banana-app/app.js
@@ -1,11 +1,10 @@
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
 function getTranslationURL(input) {
@@ -20,7 +19,6 @@ function errorHandler(error) {
 
 function clickHandler() {
     var inputText = txtInput.value; // taking input
-    console.log(inputText);
 
     // calling server for processing
     fetch(getTranslationURL(inputText))
@@ -32,4 +30,4 @@ function clickHandler() {
         .catch(errorHandler)
 };
 
-btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("click", clickHandler)
